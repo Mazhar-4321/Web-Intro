93c8120e3d11ee5f8d5e1363fff4e97ac86b94ab
Mazhar-4321 <syedmazharali742@gmail.com>
2023-01-15 13:10:14 +0530

    ADDED: Code To Edit Contact

--- Main.js
+++ Main.js
@@ -18,9 +18,9 @@ let lastName = process.argv[3];
 let city = process.argv[4];
 let state = process.argv[5];
 let zip = process.argv[6];
-let address = process.argv[7];
-let phoneNumber = process.argv[8];
-let email = process.argv[9];
+let phoneNumber = process.argv[7];
+let email = process.argv[8];
+let address = process.argv[9];
 personObject = createAddressBook(firstName, lastName, city, state, zip, phoneNumber, email, address);
 if (validateContact(firstName, lastName, city, state, zip, phoneNumber, email, address)) {
     console.log("valid contact details");
@@ -28,7 +28,7 @@ if (validateContact(firstName, lastName, city, state, zip, phoneNumber, email, a
 } else {
     console.log("Invalid Contact Details")
 }
-
+editContact("Mazhar","Ali", "Hyderabad", state, zip, phoneNumber, email, address)
 function createAddressBook(firstName, lastName, city, state, zip, phoneNumber, email, address) {
     return new Person(firstName, lastName, city, state, zip, phoneNumber, email, address);
 }
@@ -38,7 +38,22 @@ function validateContact(firstName, lastName, city, state, zip, phoneNumber, ema
             city.match(/[A-Z][a-z]{2}/) == null ? false :
                 state.match(/[A-Z][a-z]{2}/) == null ? false :
                     zip.match(/[1-9][0-9]{5}/) == null ? false :
-                        phoneNumber.match(/^(91 [1-9][0-9]{9})$/) == null ? false :
+                        phoneNumber.match(/[1-9][0-9]{9}/) == null ? false :
                             email.match(/^(([a-zA-z]{3,}(([.+-@]?[0-9_+-]{1,})|([0-9_+-]*)))(\.[a-z]{2,})?@([a-z]{2,}|[0-9]{1})\.[a-z]{2,}(\.[a-z]{2,})?)$/) == null ? false :
                                 address.match(/[A-Z][a-z]{2}/) == null ? false : true;
+}
+function editContact(firstName, lastName, city, state, zip, phoneNumber, email, address){
+   let contact= addressBookArray.filter(e=>e.firstName===firstName&&e.lastName===lastName)
+   //console.log(contact)
+   if(contact.length==0){
+    console.log("Contact Doesn't Exists")
+   }else{
+   contact.city=city
+   contact.state=state
+   contact.zip=zip
+   contact.phoneNumber=phoneNumber
+   contact.email=email
+   contact.address=address
+   console.log(addressBookArray)
+   }
 }
