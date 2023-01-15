33e7fe60e0d5c95c3abfa5f255f2151fb3f8d6ee
Mazhar-4321 <syedmazharali742@gmail.com>
2023-01-15 12:07:08 +0530

    ADDED: Code To Create Address Book

--- Main.js
+++ Main.js
@@ -1,1 +1,26 @@
 console.log("Welcome To Address Book Program")
+let personObject = null;
+let firstName = process.argv[2];
+let lastName = process.argv[3];
+let city = process.argv[4];
+let state = process.argv[5];
+let zip = process.argv[6];
+let address = process.argv[7];
+let phoneNumber = process.argv[8];
+let email = process.argv[9];
+createAddressBook(firstName, lastName, city, state, zip, phoneNumber, email, address)
+class Person {
+    constructor(firstName, lastName, city, state, zip, phoneNumber, email, address) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.city = city
+        this.state = state
+        this.zip = zip
+        this.phoneNumber = phoneNumber
+        this.email = email
+        this.address = address
+    }
+}
+function createAddressBook(firstName, lastName, city, state, zip, phoneNumber, email, address) {
+    personObject = new Person(firstName, lastName, city, state, zip, phoneNumber, email, address);
+}
